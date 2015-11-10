98759dab8d8db4b4e1bfda2c0b25973f1d2705cf
Antti Myyrä <antti@myyrae.fi>
2015-11-10 19:59:16 +0200

    What fun things can happen when you change favicons filename

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.set('protectedSecret', config.secret);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
